2e7df0b68d00dc15e8054d3b21d9542ec83a3b8b
Samuel Mejia <samuelmejiasierra@gmail.com>
2025-06-22 00:01:09 -0500

    prueba_6

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark overflow-hidden h-screen">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark ">
 
 <head>
     <meta charset="utf-8">
@@ -19,7 +19,7 @@
     @inertiaHead
 </head>
 
-<body class="font-sans antialiased h-screen">
+<body class="font-sans antialiased">
     @inertia
 </body>
 
